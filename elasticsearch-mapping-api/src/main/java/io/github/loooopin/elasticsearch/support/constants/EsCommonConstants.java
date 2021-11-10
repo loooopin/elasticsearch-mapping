010f7517ce3502272740f97717ba34d97d04a8f6
loooopin <136468158@qq.com>
2021-11-10 15:14:40 +0800

    只保留client包

--- elasticsearch-mapping-api/src/main/java/io/github/loooopin/elasticsearch/support/constants/EsCommonConstants.java
+++
@@ -1,7 +0,0 @@
-package io.github.loooopin.elasticsearch.support.constants;
-
-public interface EsCommonConstants {
-    String RESPONSE_TOTAL_ELEMENT_KEY = "totalElement";
-    String DEFAULT_COUNT_FIELD = "id";
-    String HAS_NOT_INDEX_NAME = "NONE_INDEX";
-}
